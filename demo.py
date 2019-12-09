c8673a3a15f17bbe9ea0b2adb5985105c07528e8
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-09 14:30:59 +0700

    a

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 
 from efficientnet_pytorch import EfficientNet
-model = EfficientNet.from_name('efficientnet-b0')
+net = EfficientNet.from_name('efficientnet-b0')
 img_size=512
 data = torch.zeros((1, 3, img_size, img_size))
 output = net(data)
