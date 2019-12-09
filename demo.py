d7832a9e8ccfd385aef242f3fe78a346133bcca8
lehuyhunghedspi <yeuvanhoanhat@gmail.com>
2019-12-09 15:01:27 +0700

    a

--- demo.py
+++ demo.py
@@ -9,7 +9,7 @@ net = EfficientNet.from_name('efficientnet-b0')
 img_size=512
 data = torch.zeros((1, 3, img_size, img_size))
 output,temp_results = net(data)
-
+print(output.shape)
+print('=======')
 for result in temp_results:
 	print(result.shape)
-print(output.shape)
